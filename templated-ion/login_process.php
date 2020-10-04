c99dce098705f878174bdd30c24df033e3f41b9f
Ung Wei Liang <liang2010@hotmail.my>
2020-10-04 17:21:12 +0800

    update admin and user login separate

--- templated-ion/login_process.php
+++ templated-ion/login_process.php
@@ -10,7 +10,12 @@
     $result = $con->query($query);
 
     if($result->num_rows > 0){
-        echo "<script type='text/javascript'>alert('Login Successful');window.location='index.php';</script>";
+        $row = $result->assoc();
+        if($row["usertype"] == "admin"){
+            echo "<script type='text/javascript'>alert('Login Successful');window.location='adminindex.php';</script>";
+        } else {
+            echo "<script type='text/javascript'>alert('Login Successful');window.location='userindex.php';</script>";
+        }
     } else {
         echo "<script type='text/javascript'>alert('Username or Password is incorrect');window.location='login.php';</script>";
     }
