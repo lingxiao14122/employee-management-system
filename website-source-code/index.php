76ed8d997f859f75a374631784074e2b49c21969
JarvisLing <clx9386@gmail.com>
2020-11-27 19:43:34 +0800

    smaal edit

--- website-source-code/index.php
+++ website-source-code/index.php
@@ -5,187 +5,148 @@
 	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
 -->
 <html>
-  <head>
-    <title>CapstoneProject</title>
-    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
-    <meta name="description" content="" />
-    <meta name="keywords" content="" />
-    <!--[if lte IE 8]><script src="js/html5shiv.js"></script><![endif]-->
-    <script src="js/jquery.min.js"></script>
-    <script src="js/skel.min.js"></script>
-    <script src="js/skel-layers.min.js"></script>
-    <script src="js/init.js"></script>
-    <noscript>
-      <link rel="stylesheet" href="css/skel.css" />
-      <link rel="stylesheet" href="css/style.css" />
-      <link rel="stylesheet" href="css/style-xlarge.css" />
-    </noscript>
-  </head>
-  <body id="top">
+
+<head>
+  <title>CapstoneProject</title>
+  <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+  <meta name="description" content="" />
+  <meta name="keywords" content="" />
+  <!--[if lte IE 8]><script src="js/html5shiv.js"></script><![endif]-->
+  <script src="js/jquery.min.js"></script>
+  <script src="js/skel.min.js"></script>
+  <script src="js/skel-layers.min.js"></script>
+  <script src="js/init.js"></script>
+  <noscript>
+    <link rel="stylesheet" href="css/skel.css" />
+    <link rel="stylesheet" href="css/style.css" />
+    <link rel="stylesheet" href="css/style-xlarge.css" />
+  </noscript>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA==" crossorigin="anonymous" />
+  <style>
+    .icon:before {
+      font-family: "Font Awesome 5 Free";
+      font-style: normal;
+      font-weight: 900;
+    }
+  </style>
+</head>
+
+<body id="top">
 
   <?php include("navbar_top.php") ?>
 
-    <!-- Banner -->
-    <section id="banner">
-      <div class="inner">
-        <h2>Employee Management</h2>
-        <p>
-          Sign In Now to Make Attandance and receive Updates of your team.
-          
-        </p>
-        <ul class="actions">
-          <li><a href="login.php" class="button big special">Login In</a></li>
-          <li>
-            <a href="testing-dashboard.php" class="button big alt">Erm dummy button</a>
-          </li>
-        </ul>
-      </div>
-    </section>
+  <!-- Banner -->
+  <section id="banner">
+    <div class="inner">
+      <h2>Employee Management</h2>
+      <p>
+        Sign In Now to Clock In and Receive Updates of Your Team.
 
-    <!-- One -->
-    <section id="one" class="wrapper style1">
-      <header class="major">
-        <h2>employee management software</h2>
-        <p>Checkout New Tools for Your Needs</p>
-      </header>
-      <div class="container">
-        <div class="row">
-          <div class="4u">
-            <section class="special box">
-              <i class="icon fa-area-chart major"></i>
-              <h3>Apply Leave</h3>
-              <p>
-                Click here and use the new tools to apply for leave We will work
-                toward better work environment.
-              </p>
-            </section>
-          </div>
-          <div class="4u">
-            <section class="special box">
-              <i class="icon fa-refresh major"></i>
-              <h3>Blandit quis curae</h3>
-              <p>
-                Eu non col commodo accumsan ante mi. Commodo consectetur sed mi
-                adipiscing accumsan ac nunc tincidunt lobortis.
-              </p>
-            </section>
-          </div>
-          <div class="4u">
-            <section class="special box">
-              <i class="icon fa-cog major"></i>
-              <h3>Amet sed accumsan</h3>
-              <p>
-                Eu non col commodo accumsan ante mi. Commodo consectetur sed mi
-                adipiscing accumsan ac nunc tincidunt lobortis.
-              </p>
-            </section>
-          </div>
+      </p>
+      <ul class="actions">
+
+        <?php
+        if (isset($_COOKIE["PHPSESSID"])) {
+          echo "<li><a href='logout_process.php' class='button big special'>Login Out</a></li>";
+        } else {
+          echo "<li><a href='login.php' class='button big special'>Login Out</a></li>";
+        }
+
+
+        ?>
+
+      </ul>
+    </div>
+  </section>
+
+  <!-- One -->
+  <section id="one" class="wrapper style1">
+    <header class="major">
+      <h2>employee management software</h2>
+      <p>Checkout New Tools for Your Needs</p>
+    </header>
+    <div class="container">
+      <div class="row">
+        <div class="4u">
+          <section class="special box">
+            <!-- <i class="icon fa-user-clock major"></i> -->
+            <i class="icon fa-user-clock fas major"></i>
+            <h3>Clock In</h3>
+            <p>
+              Use brand new clock in tool to process clock in and clock out
+              .
+            </p>
+          </section>
+        </div>
+        <div class="4u">
+          <section class="special box">
+            <i class="icon fa-sign-out-alt fas major"></i>
+            <h3>Apply Leave</h3>
+            <p>
+              Try here and use the new tools to apply for leave .
+            </p>
+          </section>
+        </div>
+        <div class="4u">
+          <section class="special box">
+            <i class="icon fa-cog major"></i>
+            <h3>Setting</h3>
+            <p>
+              Advanced tools to prepare you in revolution 4.0.
+            </p>
+          </section>
         </div>
       </div>
-    </section>
+    </div>
+  </section>
 
-    <!-- Two -->
-    <section id="two" class="wrapper style2">
-      <header class="major">
-        <h2>Commodo accumsan aliquam</h2>
-        <p>Amet nisi nunc lorem accumsan</p>
-      </header>
-      <div class="container">
-        <div class="row">
-          <div class="6u">
-            <section class="special">
-              <a href="#" class="image fit"
-                ><img src="images/pic01.jpg" alt=""
-              /></a>
-              <h3>Mollis adipiscing nisl</h3>
-              <p>
-                Eget mi ac magna cep lobortis faucibus accumsan enim lacinia
-                adipiscing metus urna adipiscing cep commodo id. Ac quis arcu
-                amet. Arcu nascetur lorem adipiscing non faucibus odio nullam
-                arcu lobortis. Aliquet ante feugiat. Turpis aliquet ac posuere
-                volutpat lorem arcu aliquam lorem.
-              </p>
-              <ul class="actions">
-                <li><a href="#" class="button alt">Learn More</a></li>
-              </ul>
-            </section>
-          </div>
-          <div class="6u">
-            <section class="special">
-              <a href="#" class="image fit"
-                ><img src="images/pic02.jpg" alt=""
-              /></a>
-              <h3>Neque ornare adipiscing</h3>
-              <p>
-                Eget mi ac magna cep lobortis faucibus accumsan enim lacinia
-                adipiscing metus urna adipiscing cep commodo id. Ac quis arcu
-                amet. Arcu nascetur lorem adipiscing non faucibus odio nullam
-                arcu lobortis. Aliquet ante feugiat. Turpis aliquet ac posuere
-                volutpat lorem arcu aliquam lorem.
-              </p>
-              <ul class="actions">
-                <li><a href="#" class="button alt">Learn More</a></li>
-              </ul>
-            </section>
-          </div>
+  <!-- Two -->
+  <section id="two" class="wrapper style2">
+    <header class="major">
+      <h2>Commodo accumsan aliquam</h2>
+      <p>Amet nisi nunc lorem accumsan</p>
+    </header>
+    <div class="container">
+      <div class="row">
+        <div class="6u">
+          <section class="special">
+            <a href="#" class="image fit"><img src="images/pic04.jpg" alt="" /></a>
+            <h3>Software develpoed in house</h3>
+            <p>
+              Our software development team created this application to facilitate 
+              the automation and embracing technology.In regards to the new project, 
+              changes are made to the code base. This is a good thing, 
+               this project is awesome.
+            </p>
+            <ul class="actions">
+              <li><a href="#" class="button alt">Learn More</a></li>
+            </ul>
+          </section>
+        </div>
+        <div class="6u">
+          <section class="special">
+            <a href="#" class="image fit"><img src="images/pic02.jpg" alt="" /></a>
+            <h3>Neque ornare adipiscing</h3>
+            <p>
+              Eget mi ac magna cep lobortis faucibus accumsan enim lacinia
+              adipiscing metus urna adipiscing cep commodo id. Ac quis arcu
+              amet. Arcu nascetur lorem adipiscing non faucibus odio nullam
+              arcu lobortis. Aliquet ante feugiat. Turpis aliquet ac posuere
+              volutpat lorem arcu aliquam lorem.
+            </p>
+            <ul class="actions">
+              <li><a href="#" class="button alt">Learn More</a></li>
+            </ul>
+          </section>
         </div>
       </div>
-    </section>
+    </div>
+  </section>
 
-    <!-- Three -->
-    <section id="three" class="wrapper style1">
-      <div class="container">
-        <div class="row">
-          <div class="8u">
-            <section>
-              <h2>Mollis ut adipiscing</h2>
-              <a href="#" class="image fit"
-                ><img src="images/pic03.jpg" alt=""
-              /></a>
-              <p>
-                Vis accumsan feugiat adipiscing nisl amet adipiscing accumsan
-                blandit accumsan sapien blandit ac amet faucibus aliquet
-                placerat commodo. Interdum ante aliquet commodo accumsan vis
-                phasellus adipiscing. Ornare a in lacinia. Vestibulum accumsan
-                ac metus massa tempor. Accumsan in lacinia ornare massa amet. Ac
-                interdum ac non praesent. Cubilia lacinia interdum massa
-                faucibus blandit nullam. Accumsan phasellus nunc integer.
-                Accumsan euismod nunc adipiscing lacinia erat ut sit. Arcu amet.
-                Id massa aliquet arcu accumsan lorem amet accumsan commodo odio
-                cubilia ac eu interdum placerat placerat arcu commodo lobortis
-                adipiscing semper ornare pellentesque.
-              </p>
-            </section>
-          </div>
-          <div class="4u">
-            <section>
-              <h3>Magna massa blandit</h3>
-              <p>
-                Feugiat amet accumsan ante aliquet feugiat accumsan. Ante
-                blandit accumsan eu amet tortor non lorem felis semper. Interdum
-                adipiscing orci feugiat penatibus adipiscing col cubilia lorem
-                ipsum dolor sit amet feugiat consequat.
-              </p>
-              <ul class="actions">
-                <li><a href="#" class="button alt">Learn More</a></li>
-              </ul>
-            </section>
-            <hr />
-            <section>
-              <h3>Ante sed commodo</h3>
-              <ul class="alt">
-                <li><a href="#">Erat blandit risus vis adipiscing</a></li>
-                <li><a href="#">Tempus ultricies faucibus amet</a></li>
-                <li><a href="#">Arcu commodo non adipiscing quis</a></li>
-                <li><a href="#">Accumsan vis lacinia semper</a></li>
-              </ul>
-            </section>
-          </div>
-        </div>
-      </div>
-    </section>
 
-    <?php include("footer.php") ?>
 
-  </body>
+  <?php include("footer.php") ?>
+
+</body>
+
 </html>
--- website-source-code/navbar_top.php
+++ website-source-code/navbar_top.php
@@ -1,24 +1,25 @@
-
 <!--this is the navigation bar on top of every webpage, please include in all pages-->
 
 <!-- Header -->
 <header id="header" class="skel-layers-fixed">
-      <h1><a href="index.php">EMPLOYEE MANAGEMENT SOFTWARE</a></h1>
-      <nav id="nav">
-        <ul>
-          <li><a href="index.php">Home</a></li>
-          <li><a href="testing-user-dashboard.php">Left Sidebar</a></li>
-          <li><a href="template-right-sidebar.html">Right Sidebar</a></li>
-          <li><a href="template-no-sidebar.html">No Sidebar</a></li>
+  <h1><a href="index.php">EMPLOYEE MANAGEMENT SOFTWARE</a></h1>
+  <nav id="nav">
+    <ul>
+      <li><a href="index.php">Home</a></li>
 
-          <?php
-          if(!isset($_COOKIE["PHPSESSID"])){
-            echo "<li><a href=\"login.php\" class=\"button special\">Log In</a></li>";
-          } else {
-            echo "<li><a href=\"logout_process.php\" class=\"button special\">Log Out</a></li>";
-          }
-          ?>
-        </ul>
-      </nav>
-    </header>
-    
+      <?php
+
+      if (isset($_COOKIE["PHPSESSID"])) {
+        echo "<li><a href='user-dashboard.php'>Dashboard</a></li>";
+      }
+
+      if (!isset($_COOKIE["PHPSESSID"])) {
+        echo "<li><a href=\"login.php\" class=\"button special\">Log In</a></li>";
+      } else {
+        echo "<li><a href=\"logout_process.php\" class=\"button special\">Log Out</a></li>";
+      }
+
+      ?>
+    </ul>
+  </nav>
+</header>
